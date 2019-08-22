49502a3e86dd7c53992d9c303bef18978635fedd
suttisak <suttisak1297@gmail.com>
2019-08-22 15:10:59 +0700

    Lab5

--- src/Lab5/Lab5_2.java
+++ src/Lab5/Lab5_2.java
@@ -3,23 +3,25 @@ package Lab5;
 import java.util.Scanner;
 
 public class Lab5_2 {
-    public static void main(String[] args) {50
+    public static void main(String[] args) {
+
 
         Scanner sc = new Scanner(System.in);
-        final  double PI = 3.141;
+        final double PI = 3.141;
 
         System.out.println("กรุณากรอกตัวเลขของรัศมี:");
         double r = sc.nextDouble();
 
-        double Area  = PI * r*r;
+        double Area = PI * r * r;
         System.out.println("พื้นที่ผิวของวงกลม: " + Area);
 
-        double c = 2* PI *r;
-        System.out.println("เส้นรอบวงกลม: "+c);
+        double c = 2 * PI * r;
+        System.out.println("เส้นรอบวงกลม: " + c);
 
-        double v = (4/3) *PI*r*r*r;
-        System.out.println("ปริมาตรวงกลม: "+v);
-    }
+        double v = (4 / 3) * PI * r * r * r;
+        System.out.println("ปริมาตรวงกลม: " + v);
 
     }
+}
+
 
---
+++ src/Lab6/Lab6_1.java
@@ -0,0 +1,18 @@
+package Lab6;
+
+public class Lab6_1 {
+    public static void main(String[] args) {
+        int temp;
+        int far;
+        int cel;
+
+        temp = 25;
+
+        far = temp * 9 / 5 + 32; //convert Celsius to Fahrenheit
+        cel = (far - 32) * 5 / 9; // convert Fahrenheit to Celsius
+
+        System.out.println("Celsius --> Fahrenheit: " + far);
+        System.out.println("Fahrenheit --> Celsius: " + cel);
+    }
+}
+
