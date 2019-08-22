8dbb68fdb3f1ad9bf2422d2f4dde290cf7fa0351
suttisak <suttisak1297@gmail.com>
2019-08-22 14:50:02 +0700

    Lab6

--- src/Lab6/ex1.java
+++ src/Lab6/ex1.java
@@ -1,4 +1,14 @@
 package Lab6;
 
 public class ex1 {
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        System.out.println(x);
+        System.out.println(x++);
+        System.out.println(x);
+        x--;
+        --x;
+    }
 }
