8c6d17bea6ed0aa780012474ab93ed3f586a9182
suttisak <suttisak1297@gmail.com>
2019-08-22 13:54:16 +0700

    Lab5

---
+++ src/Lab5/Lab5_1.java
@@ -0,0 +1,23 @@
+package Lab5;
+
+public class Lab5_1 {
+    public static void main(String[] args) {
+        //integer
+        int x;
+        x=10;
+        System.out.println(x);
+
+
+        //floating
+        float f = 10f;
+        double m = 12.5;
+        System.out.println(f +" "+m);
+
+        //string
+        String msg = "Computer Programming";
+        System.out.println(msg);
+        System.out.println(msg.length());
+        System.out.println(msg.toUpperCase());
+        System.out.println(msg.substring(1));
+    }
+}
---
+++ src/Lab5/Lab5_3.java
@@ -0,0 +1,19 @@
+package Lab5;
+
+import java.util.Scanner;
+
+public class Lab5_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int day;
+        long hr,minute,seconds;
+        System.out.println("Enter second:");
+        long input_second =sc.nextLong(); //1000000 s
+
+        day = (int) (input_second / (60 * 60 * 24));
+        System.out.println("Day: "+day);
+
+        hr = (input_second - (day * 60 * 60 * 24)) / (60 * 60);
+        System.out.println("H: "+day);
+    }
+}
