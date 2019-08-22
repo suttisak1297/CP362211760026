4b4f235afa3d4b4b6e4e3281ad99535f222e2b19
suttisak <suttisak1297@gmail.com>
2019-08-22 16:12:21 +0700

    Lab6

---
+++ src/Lab6/Lab6_2.java
@@ -0,0 +1,15 @@
+package Lab6;
+
+import java.util.Scanner;
+
+public class Lab6_2 {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     double i;
+     System.out.println("Input length in inch: ");
+     i = sc.nextDouble();
+     double m = i*0.0254;
+
+     System.out.println(i+ " inch is equal to "+m+" meters.");
+    }
+}
