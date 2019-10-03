6fa146d1c8e73df9a9d033c1a389a46f2ad80106
suttisak <suttisak1297@gmail.com>
2019-10-03 16:27:49 +0700

    Lab6

---
+++ src/Lab7/Ex1.java
@@ -0,0 +1,18 @@
+package Lab7;
+
+public class Ex1 {
+    public static void main(String[] args) {
+       int  x =100, y =20;
+
+       //if
+        if (x < y && x ==100 || y >10) {
+            //statements
+            System.out.println("x less than y. ");
+        }else {
+            System.out.println("x more than y. ");
+        }
+
+
+
+    }//main
+}//class
