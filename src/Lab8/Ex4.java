87715c189c384c319c35bf5e4b45211b5485646d
suttisak <suttisak1297@gmail.com>
2019-10-27 15:46:13 +0700

    Lab6

---
+++ src/Lab8/Ex4.java
@@ -0,0 +1,16 @@
+package Lab8;
+
+public class Ex4 {
+    public static void main(String[] args) {
+
+       max(5,10);
+       max(5.5,10.10);
+       max(5,10.10);
+
+
+    }//main
+    public static void max (int x,int y){}
+    public static void max (double x,double y){}
+
+
+}//class
